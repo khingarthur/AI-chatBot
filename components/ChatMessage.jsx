f3313cd1162f62148526522108387a0f7bcbfffd
Khingarthur <khingarthur01@gmail.com>
2026-06-20 18:11:02 +0000

    Chat Message component

--- components/ChatMessage.jsx
+++ components/ChatMessage.jsx
@@ -1,20 +1,41 @@
 export const ChatMessage = ({message, sender}) =>{
     
     // const imagePath = sender === "user"? "/shirt.png" : "/person.svg" ;
-    
-        return (
-            <div>
-                { sender === "user" && (
-                    <img src="/person.png" 
-                    alt="" 
-                    className="img-fluid chat-avatar" />)
-                }
-                <span>{message}</span>
-                { sender === "robot" && (
-                    <img src="/shirt.png" 
-                    alt="" 
-                    className="img-fluid chat-avatar" />)
-                }
-            </div>
-        )
+    const isUser = sender === 'user';
+
+    return (
+        <div className={`d-flex mb-3 ${isUser ? 'justify-content-end' : 'justify-content-start'}`}>
+
+        {sender === "robot" && (
+            <img 
+            src="shirt.png" 
+            alt="robot"
+            className="rounded-circle me-2 align-self-end"
+            width="40" 
+            height="40"
+            />
+        )}
+
+        <div 
+            className={`p-3 shadow-sm ${isUser ? 'bg-primary text-white' : 'bg-light text-dark'}`}
+            style={{
+            maxWidth: '75%',
+            borderRadius: '15px'
+            }}
+        >
+            {message}
+        </div>
+
+        {sender === "user" && (
+            <img 
+            src="/person.png" 
+            alt="user"
+            className="img-fluid chat-avatar rounded-circle ms-2 align-self-end" 
+            width="40" 
+            height="40"
+            />
+        )}
+
+        </div>
+    );
 }
