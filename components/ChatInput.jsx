d952086545a6a9b0381096916d249e3d38dee7ef
Khingarthur <khingarthur01@gmail.com>
2026-06-20 18:10:21 +0000

    Chat input component

--- components/ChatInput.jsx
+++ components/ChatInput.jsx
@@ -1,10 +1,19 @@
 
 
-export const ChatInput = ({onClick, onChange, value}) =>{
+export const ChatInput = ({onClick, onChange, value, onKeyDown, disabled}) =>{
     return (
-        <div>
-            <input type="text" placeholder="Enter your message" onChange={onChange} value={value} />
-            <button className="btn btn-primary" onClick={onClick} >Send</button>
+        <div className="input-group">
+            <input 
+                type="text" 
+                placeholder="Enter your message" 
+                onChange={onChange} 
+                value={value}
+                onKeyDown={onKeyDown}
+                disabled ={disabled}
+                className="form-control form-control-lg"
+            />
+
+            <button className="btn btn-primary px-4 ms-3" onClick={onClick} >Send</button>
         </div>
     )
 }
